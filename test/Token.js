2e7573689c6c968e328d57f06bcafa15f49d7ca3
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-08-10 07:38:18 -0400

    test-file

---
+++ test/Token.js
@@ -0,0 +1,15 @@
+const { expect } = require("chai");
+
+describe("Token", () => {
+  let tokenSupply = "100";
+  let token;
+  let owner;
+  let addr1;
+  let addr2;
+
+  beforeEach(async () => {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy(tokenSupply);
+  });
+});
